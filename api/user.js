f7cdd228e7133d52459460bfb8c40aa9ecda4616
David Pirek <dpirek@gmail.com>
2025-10-04 08:09:06 -0400

    loading sample users

--- api/user.js
+++ api/user.js
@@ -1,11 +1,13 @@
 //const { decryptCookie, getCookie } = require('./utils/auth');
+async function getUsers() {
+  const url = `https://dummyjson.com/users`
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
 
 async function userApi(req) {
-  return {
-    arg: req.params,
-    url: req.url,
-    method: req.method
-  };
+  return await getUsers();
 }
 
 module.exports = userApi;
